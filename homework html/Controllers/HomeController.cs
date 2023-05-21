09c9895a1a09fab2e00c8812635247b934aedb2d
omar-maasher <omar15448@gmail.com>
2023-05-21 23:59:40 +0300

    html homework

---
+++ homework html/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using homework_html.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace homework_html.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Single()
+        {
+            return View();
+        }
+    }
+}
---
+++ homework html/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace homework_html.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
